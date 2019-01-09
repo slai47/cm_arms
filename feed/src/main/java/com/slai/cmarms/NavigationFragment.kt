b241776ef6c9178976545de2296ea82feef86053
Harrison Frede <harrisonfrede@gmail.com>
2019-01-08 19:29:40 -0500

    Fixing the navigation bar shrinking issue

--- feed/src/main/java/com/slai/cmarms/NavigationFragment.kt
+++ feed/src/main/java/com/slai/cmarms/NavigationFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.ashokvarma.bottomnavigation.BottomNavigationBar
+import com.ashokvarma.bottomnavigation.BottomNavigationItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.slai.cmarms.model.NavigationEvent
 import com.slai.cmarms.model.NavigationTransitionEvent
@@ -22,28 +25,67 @@ class NavigationFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         EventBus.getDefault().register(this)
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        setupNavigation()
         val fragment = fragmentManager!!.findFragmentById(R.id.nav_container)
         if(fragment == null)
             fragmentManager!!.beginTransaction().add(R.id.nav_container, FeedFragment()).commit()
     }
 
+    fun setupNavigation(){
+        navigation.addItem(BottomNavigationItem(R.drawable.ic_dashboard, R.string.title_home).setActiveColorResource(R.color.colorPrimary))
+        navigation.addItem(BottomNavigationItem(R.drawable.ic_filter, R.string.title_search).setActiveColorResource(R.color.colorPrimary))
+        navigation.addItem(BottomNavigationItem(R.drawable.ic_settings, R.string.title_settings).setActiveColorResource(R.color.colorPrimary))
+
+        navigation.initialise()
+        navigation.selectTab(0)
+        navigation.setTabSelectedListener( object : BottomNavigationBar.OnTabSelectedListener {
+            override fun onTabReselected(position: Int) {
+            }
+
+            override fun onTabUnselected(position: Int) {
+            }
+
+            override fun onTabSelected(position: Int) {
+                val replace : Boolean
+                val frag : Fragment
+                val currentFrag = fragmentManager!!.findFragmentById(R.id.nav_container)
+
+                if(position == 0) {
+                    frag = FeedFragment()
+                    replace = currentFrag !is FeedFragment
+                } else if (position == 1){
+                    frag = FilterFragment()
+                    replace = currentFrag !is FilterFragment
+                } else {
+                    frag = SettingsFragment()
+                    replace = currentFrag !is SettingsFragment
+                }
+
+                if(replace)
+                    fragmentManager!!.beginTransaction().replace(R.id.nav_container, frag!!).commit()
+            }
+        })
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                fragmentManager!!.beginTransaction().replace(R.id.nav_container, FeedFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_search -> {
-                fragmentManager!!.beginTransaction().replace(R.id.nav_container, FilterFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_settings -> {
-                fragmentManager!!.beginTransaction().replace(R.id.nav_container, SettingsFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
+        val replace : Boolean
+        val frag : Fragment
+        val currentFrag = fragmentManager!!.findFragmentById(R.id.nav_container)
+
+        if(item.itemId == R.id.navigation_home) {
+            frag = FeedFragment()
+            replace = currentFrag !is FeedFragment
+        } else if (item.itemId == R.id.navigation_search){
+            frag = FilterFragment()
+            replace = currentFrag !is FilterFragment
+        } else {
+            frag = SettingsFragment()
+            replace = currentFrag !is SettingsFragment
         }
-        false
+
+        if(replace)
+            fragmentManager!!.beginTransaction().replace(R.id.nav_container, frag!!).commit()
+        return@OnNavigationItemSelectedListener true
     }
 
     override fun onPause() {
@@ -53,10 +95,11 @@ class NavigationFragment : Fragment() {
 
     @Subscribe
     fun onNavigationEvent(event : NavigationTransitionEvent) {
-        when (event.navTo){
-            NavigationEvent.FEED -> navigation.selectedItemId = R.id.navigation_home
-            NavigationEvent.SEARCH -> navigation.selectedItemId = R.id.navigation_search
-            NavigationEvent.SETTINGS -> navigation.selectedItemId = R.id.navigation_settings
-        }
+//        when (event.navTo){
+//            NavigationEvent.FEED -> navigation.selectedItemId = R.id.navigation_home
+//            NavigationEvent.SEARCH -> navigation.selectedItemId = R.id.navigation_search
+//            NavigationEvent.SETTINGS -> navigation.selectedItemId = R.id.navigation_settings
+//        }
     }
+
 }
--- feed/src/main/java/com/slai/cmarms/SettingsFragment.kt
+++ feed/src/main/java/com/slai/cmarms/SettingsFragment.kt
@@ -52,8 +52,4 @@ class SettingsFragment : PreferenceFragmentCompat() {
             true
         }
     }
-
-
-
-
 }
