5c594cffb42bb52bb9157edeaa84be21dcf777ce
Harrison Frede <harrisonfrede@gmail.com>
2019-01-12 18:51:48 -0500

    Cleaning up FilterFragment with MVP

--- feed/src/main/java/com/slai/cmarms/FilterFragment.kt
+++ feed/src/main/java/com/slai/cmarms/FilterFragment.kt
@@ -1,31 +1,22 @@
 package com.slai.cmarms
 
 import android.Manifest
-import android.app.AlertDialog
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
-import com.slai.cmarms.adapters.FilterAdapter
-import com.slai.cmarms.backend.GeocoderApi
 import com.slai.cmarms.model.*
-import com.slai.cmarms.view.DialogRecyclerView
+import com.slai.cmarms.presenters.FilterPresenter
 import com.slai.cmarms.viewmodel.CmarmsViewModel
 import kotlinx.android.synthetic.main.fragment_nav.*
 import kotlinx.android.synthetic.main.snippet_filter.*
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.EventBus
@@ -33,18 +24,13 @@ import org.greenrobot.eventbus.Subscribe
 
 class FilterFragment : Fragment() {
 
-    val REQUEST_CODE = 47
     val HAS_CHECKED_LOCATION = "hasCheckedLocationPermissions1"
 
     val viewModel by lazy { ViewModelProviders.of(activity!!).get(CmarmsViewModel::class.java) }
 
-    private lateinit var fusedLocationClient : FusedLocationProviderClient
-
-    private val filterDB = FiltersDataHolder()
-
-    lateinit var geocoderApi : GeocoderApi
-
     var permissionsAskSnackbar : Snackbar? = null
+
+    val presenter = FilterPresenter(this)
 
     val pref by lazy { context!!.getSharedPreferences(PrefUtils.PREFERENCE_FIELD, Context.MODE_PRIVATE) }
 
@@ -56,17 +42,12 @@ class FilterFragment : Fragment() {
         super.onResume()
         EventBus.getDefault().register(this)
 
-        geocoderApi = GeocoderApi(context!!)
+        presenter.createDialogs(filter_category, filter_caliber, filter_firearm_type, filter_action_type)
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        presenter.setupSearchReset(filter_search, filter_reset)
 
-        setupDialog(filter_category, filterDB.categories, getString(R.string.categories_title), PrefUtils.PREF_CATEGORY)
-        setupDialog(filter_caliber, filterDB.calibers, getString(R.string.caliber_title), null)
-        setupDialog(filter_firearm_type, filterDB.firearmTypes, getString(R.string.firearm_type_title), PrefUtils.PREF_FIREARM_TYPE)
-        setupDialog(filter_action_type, filterDB.actions, getString(R.string.action_type_title), PrefUtils.PREF_ACTION_TYPE)
-
-        setupSearchReset()
         fillQueryEditTexts()
+
         updateButtonText(FilterDialogClosed(""))
 
         setupLocation()
@@ -109,7 +90,7 @@ class FilterFragment : Fragment() {
                 permissionsAskSnackbar?.dismiss()
             }
         }  else {
-            getLocation()
+            presenter.getLocation()
         }
     }
 
@@ -129,7 +110,7 @@ class FilterFragment : Fragment() {
             ) {
                 requestPermissions(
                     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_CODE
+                    FilterPresenter.REQUEST_CODE
                 )
             } else {
                 // Add alert sometime here
@@ -140,29 +121,7 @@ class FilterFragment : Fragment() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == REQUEST_CODE){
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLocation()
-            }
-        }
-    }
-
-    fun getLocation(){
-        updateLocationButton(false)
-        fusedLocationClient.lastLocation.addOnSuccessListener {location : Location ->
-            geocoderApi.getAddress(location) { address: Address? ->
-                if(address != null){
-                    val state = address.adminArea
-                    filter_location.setText(state)
-                } else {
-                    Snackbar.make(filter_caliber, "Geocode failed to hit the mark", Snackbar.LENGTH_SHORT).show()
-                }
-                updateLocationButton(true)
-            }
-        }.addOnFailureListener {
-            Snackbar.make(filter_caliber, "Location didn't find its shot.", Snackbar.LENGTH_SHORT).show()
-            updateLocationButton(true)
-        }
+        presenter.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun fillQueryEditTexts() {
@@ -180,29 +139,16 @@ class FilterFragment : Fragment() {
         }
     }
 
-    private fun setupSearchReset() {
-        filter_search.setOnClickListener {
-            setViewModelItems()
-            viewModel.clearPosts()
-            EventBus.getDefault().post(NavigationTransitionEvent(NavigationEvent.FEED))
-        }
-        filter_reset.setOnClickListener {
-            GlobalScope.launch(Dispatchers.IO) {
-                viewModel.reset(context!!)
-            }
-            viewModel.clearPosts()
-            val prefs = context?.getSharedPreferences(PrefUtils.PREFERENCE_FIELD, Context.MODE_PRIVATE)!!
-            prefs.edit().remove(PrefUtils.PREF_CATEGORY).remove(PrefUtils.PREF_ACTION_TYPE).remove(PrefUtils.PREF_FIREARM_TYPE).apply()
-            updateButtonText(FilterDialogClosed(""))
-            filter_search_text.text = null
-            filter_location.setText(Query.DEFAULT_LOCATION)
-            filter_low_price.text = null
-            filter_high_price.text = null
-            updateLocationButton(true)
-        }
+    fun searchReset() {
+        updateButtonText(FilterDialogClosed(""))
+        filter_search_text.text = null
+        filter_location.setText(Query.DEFAULT_LOCATION)
+        filter_low_price.text = null
+        filter_high_price.text = null
+        updateLocationButton(true)
     }
 
-    private fun setViewModelItems() {
+    fun setViewModelItems() {
         val search = filter_search_text.text.toString().trim()
         val lowPrice = filter_low_price.text.toString().trim()
         val highPrice = filter_high_price.text.toString().trim()
@@ -221,25 +167,6 @@ class FilterFragment : Fragment() {
             viewModel.query.highPrice = highPrice.toInt()
         } catch (e: Exception) {
             // show error
-        }
-    }
-
-    private fun setupDialog(button : Button, filters : ArrayList<Filter>, title : String, prefName : String?) {
-        val context = button.context
-        button.setOnClickListener {
-            val adapter = FilterAdapter(context, filters, prefName)
-            val builder = AlertDialog.Builder(context)
-            builder.setTitle(title)
-            val recyclerView = DialogRecyclerView(context)
-            recyclerView.setAdapter(adapter)
-            builder.setView(recyclerView)
-            builder.setPositiveButton(context.getString(R.string.ok)) { dialog, which ->
-                dialog.dismiss()
-            }
-            builder.setOnDismissListener {
-                EventBus.getDefault().post(FilterDialogClosed(title))
-            }
-            builder.show()
         }
     }
 
@@ -272,13 +199,11 @@ class FilterFragment : Fragment() {
         }
     }
 
-
-
     override fun onPause() {
         super.onPause()
         setViewModelItems()
         EventBus.getDefault().unregister(this)
-        geocoderApi.dispose()
+        presenter.dispose()
     }
 
     @Subscribe
@@ -292,5 +217,13 @@ class FilterFragment : Fragment() {
                 viewModel.query.filters.remove(filter.value)
             }
         }
+    }
+
+    fun showSnackbar(text : String){
+        Snackbar.make(filter_caliber, text, Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun setLocation(text : String){
+        filter_location.setText(text)
     }
 }
---
+++ feed/src/main/java/com/slai/cmarms/presenters/FilterPresenter.kt
@@ -0,0 +1,119 @@
+package com.slai.cmarms.presenters
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Location
+import android.widget.Button
+import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.slai.cmarms.FilterFragment
+import com.slai.cmarms.FiltersDataHolder
+import com.slai.cmarms.PrefUtils
+import com.slai.cmarms.R
+import com.slai.cmarms.adapters.FilterAdapter
+import com.slai.cmarms.backend.GeocoderApi
+import com.slai.cmarms.interfaces.IDispose
+import com.slai.cmarms.model.*
+import com.slai.cmarms.view.DialogRecyclerView
+import com.slai.cmarms.viewmodel.CmarmsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.greenrobot.eventbus.EventBus
+
+class FilterPresenter(var filterFragment: FilterFragment) : IDispose{
+
+    companion object {
+        val REQUEST_CODE = 47
+    }
+
+    lateinit var geocoderApi : GeocoderApi
+
+    private val filterDB = FiltersDataHolder()
+
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+
+    val viewModel by lazy { ViewModelProviders.of(filterFragment.activity!!).get(CmarmsViewModel::class.java) }
+
+    override fun dispose() {
+        geocoderApi.dispose()
+    }
+
+    init {
+        geocoderApi = GeocoderApi(filterFragment.context!!)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(filterFragment.activity!!)
+    }
+
+    fun createDialogs(vararg views : Button){
+        setupDialog(views[0], filterDB.categories, filterFragment.getString(R.string.categories_title), PrefUtils.PREF_CATEGORY)
+        setupDialog(views[1], filterDB.calibers, filterFragment.getString(R.string.caliber_title), null)
+        setupDialog(views[2], filterDB.firearmTypes, filterFragment.getString(R.string.firearm_type_title), PrefUtils.PREF_FIREARM_TYPE)
+        setupDialog(views[3], filterDB.actions, filterFragment.getString(R.string.action_type_title), PrefUtils.PREF_ACTION_TYPE)
+    }
+
+    private fun setupDialog(button : Button, filters : ArrayList<Filter>, title : String, prefName : String?) {
+        val context = button.context
+        button.setOnClickListener {
+            val adapter = FilterAdapter(context, filters, prefName)
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle(title)
+            val recyclerView = DialogRecyclerView(context)
+            recyclerView.setAdapter(adapter)
+            builder.setView(recyclerView)
+            builder.setPositiveButton(context.getString(R.string.ok)) { dialog, which ->
+                dialog.dismiss()
+            }
+            builder.setOnDismissListener {
+                EventBus.getDefault().post(FilterDialogClosed(title))
+            }
+            builder.show()
+        }
+    }
+
+    fun getLocation(){
+        filterFragment.updateLocationButton(false)
+        fusedLocationClient.lastLocation.addOnSuccessListener {location : Location ->
+            geocoderApi.getAddress(location) { address: Address? ->
+                if(address != null){
+                    val state = address.adminArea
+                    filterFragment.setLocation(state)
+                } else {
+                    filterFragment.showSnackbar(filterFragment.getString(R.string.geocode_missed))
+                }
+                filterFragment.updateLocationButton(true)
+            }
+        }.addOnFailureListener {
+            filterFragment.showSnackbar(filterFragment.getString(R.string.location_missed))
+            filterFragment.updateLocationButton(false)
+        }
+    }
+
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray){
+        if(requestCode == REQUEST_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLocation()
+            }
+        }
+    }
+
+    fun setupSearchReset(search : Button, reset : Button) {
+        search.setOnClickListener {
+            filterFragment.setViewModelItems()
+            viewModel.clearPosts()
+            EventBus.getDefault().post(NavigationTransitionEvent(NavigationEvent.FEED))
+        }
+        reset.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                viewModel.reset(filterFragment.context!!)
+            }
+            viewModel.clearPosts()
+            val prefs = filterFragment.context?.getSharedPreferences(PrefUtils.PREFERENCE_FIELD, Context.MODE_PRIVATE)!!
+            prefs.edit().remove(PrefUtils.PREF_CATEGORY).remove(PrefUtils.PREF_ACTION_TYPE).remove(PrefUtils.PREF_FIREARM_TYPE).apply()
+            filterFragment.searchReset()
+        }
+    }
+}
